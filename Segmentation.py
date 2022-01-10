82588a642a14b246665f44605bea34f0de008241
CBYellowstone <2844829687@qq.com>
2022-01-10 13:38:02 +0800

    分词折中方案

--- Segmentation.py
+++ Segmentation.py
@@ -1,24 +1,45 @@
-import MeCab
-from sudachipy import tokenizer,dictionary
+# from MeCab import Tagger
+# from sudachipy import tokenizer,dictionary
+import requests
+import json
+from urllib3 import disable_warnings
+disable_warnings()
 
+def _kuromoji(s):
+    output = list()
+    url = "https://www.atilika.org/kuromoji/rest/tokenizer/tokenize"
+    datas = {
+        'text': s,
+        'mode': 0
+    }
+    req = requests.post(url,data=datas,verify=False)
+    # var_dump(json.loads(req.content.decode('utf-8'))['tokens'])
+    for each in json.loads(req.content.decode('utf-8'))['tokens']:
+        output.append(each['surface'])
+    return output
 
-_MeCab = MeCab.Tagger("-Owakati")
-_sudachi = dictionary.Dictionary(dict="full").create()
+# _MeCab = Tagger("-Owakati")
+# _sudachi = dictionary.Dictionary(dict="full").create()
 
 def splitWords(s,m):
     output = str()
-    if(m == "MeCab"):
-        temp = _MeCab.parse(s).split()
-        for each in temp:
-            if(each != " " and each != None):
-                output += each + " | "
-        return output
-    elif(m == "sudachi"):
-        mode = tokenizer.Tokenizer.SplitMode.B
-        temp = [m.surface() for m in _sudachi.tokenize(s, mode)]
-        for each in temp:
-            if(each != " " and each != None):
-                output += each + " | "
-        return output
+    # if(m == "MeCab"):
+    #     temp = _MeCab.parse(s).split()
+    #     for each in temp:
+    #         if(each != " " and each != None):
+    #             output += each + " | "
+    #     return output
+    # elif(m == "sudachi"):
+    #     mode = tokenizer.Tokenizer.SplitMode.B
+    #     temp = [m.surface() for m in _sudachi.tokenize(s, mode)]
+    #     for each in temp:
+    #         if(each != " " and each != None):
+    #             output += each + " | "
+    #     return output
+    temp = _kuromoji(s)
+    for each in temp:
+        if(each != " " and each != None):
+            output += each + " | "
+    return output
 
-# print(splitWords('DIR EN GREYのライブ行きたい','sudachi'))
+print(splitWords('DIR EN GREYのライブ行きたい','sudachi'))
